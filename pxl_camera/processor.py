c2dcb4febb01c46a7b8ff67ba4ebe0c9c0a3e8fb
Pasko Zdilar <pasko.zdilar@pixlbrain.ai>
2020-01-21 15:00:45 +0100

    Add better docs for preprocessor

--- pxl_camera/processor.py
+++ pxl_camera/processor.py
@@ -1,5 +1,4 @@
 """
-    Class for preprocessing frames and calculating the "good" factor.
+    Class for preprocessing frames and calculating whether or not the frames
+    are good for further processing by models and stuff.
 """
-
-# Config?
