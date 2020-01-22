bf06faa2060f7f9769606be469be9a484cbc886b
Pasko Zdilar <pasko.zdilar@pixlbrain.ai>
2020-01-22 10:55:07 +0100

    Add back support for OpenCV.waitKey keycodes

---
+++ pxl_camera/util/key.py
@@ -0,0 +1,39 @@
+import enum
+
+
+class Key(enum.IntEnum):
+    """
+        Enum class identifying OpenCV waitKey() return values.
+    """
+    UNKNOWN = -2
+    NONE = -1
+
+    ENTER = 13
+    ESC = 27
+
+    A_LOWER = 97
+    B_LOWER = 98
+    C_LOWER = 99
+    D_LOWER = 100
+    E_LOWER = 101
+    F_LOWER = 102
+    G_LOWER = 103
+    H_LOWER = 104
+    I_LOWER = 105
+    J_LOWER = 106
+    K_LOWER = 107
+    L_LOWER = 108
+    M_LOWER = 109
+    N_LOWER = 110
+    O_LOWER = 111
+    P_LOWER = 112
+    Q_LOWER = 113
+    R_LOWER = 114
+    S_LOWER = 115
+    T_LOWER = 116
+    U_LOWER = 117
+    V_LOWER = 118
+    W_LOWER = 119
+    X_LOWER = 120
+    Y_LOWER = 121
+    Z_LOWER = 122
