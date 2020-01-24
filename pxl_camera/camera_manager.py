c211090369ac10a30e5e43db16cda5798fc632dd
Pasko Zdilar <pasko.zdilar@pixlbrain.ai>
2020-01-24 14:56:56 +0100

    Add device enumeration to CameraManager

--- pxl_camera/camera_manager.py
+++ pxl_camera/camera_manager.py
@@ -1,5 +1,6 @@
 """
-    Class for high-level camera management.
+    Class for high-level camera management of e-con systems See3CAM digital
+    cameras.
 
     Manages multiple cameras by serial number and provides methods for
     filtering frames.
@@ -15,9 +16,27 @@ class CameraManager(Actor):
     def __init__(self):
         super(CameraManager, self).__init__()
 
-        self.device_detector = DeviceDetector(actor=self, method='device_event')
-        self.device_mapping = self.device_detector.get_devices()
+        self.device_detector = DeviceDetector()
+        self.device_detector.start(actor=self, method='handle_device_event')
+
+        self.available_devices = self.device_detector.get_devices()
+        self.running_devices = set()
 
     def handle_device_event(self, device: str, action: str):
-        ...
-        # self.device_mapping
+        self.logger.debug(f'Handle device event: {device} {action}')
+
+        serial = self.device_detector.get_serial(device)
+        if serial is None:
+            return
+
+        if action == 'remove':
+            try:
+                self.available_devices.pop(serial)
+            except KeyError as exc:
+                self.logger.error(f'DEVICE DETECTOR ERROR: {exc}')
+
+        if action == 'add':
+            self.available_devices[serial] = device
+
+    def status(self):
+        return self.available_devices
